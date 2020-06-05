1b991df3db9d43c98197ae3a419430d16617bff1
MeetCode8 <1051329576@qq.com>
2020-06-05 22:02:53 +0800

    第三次提交文档

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div> </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
+import NavBar from "components/common/NavBar/NavBar.vue";
+import HomeSwiper from "./ChildSwiper/HomeSwiper.vue";
+import { getHomeMultidata } from "network/home.js";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    };
+  },
+  components: {
+    NavBar,
+    HomeSwiper
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   }
+};
 </script>
 
-<style scoped>
-
+<style>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
